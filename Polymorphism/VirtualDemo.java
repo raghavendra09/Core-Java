a1413ef3cd73ee19fb1fe4a6ec0837f68e13df82
raghavendra09 <raghavendratirunagari.hadoop@gmail.com>
2017-12-25 16:17:17 +0530

    Add files via upload

---
+++ Polymorphism/VirtualDemo.java
@@ -0,0 +1,21 @@
+package com.dbcore.Polymorphism;
+
+/**
+ * Project: CoreJava
+ * Author: test
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+/* File name : VirtualDemo.java */
+public class VirtualDemo
+{
+    public static void main(String [] args)
+    {
+        Salary s = new Salary("Mohd Mohtashim", "Ambehta, UP", 3, 3600.00);
+        Employee e = new Salary("John Adams", "Boston, MA", 2, 2400.00);
+        System.out.println("Call mailCheck using Salary reference --");
+        s.mailCheck();
+        System.out.println("\n Call mailCheck using Employee reference--");
+        e.mailCheck();
+    }
+}
