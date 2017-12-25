9c0d1c302ec53f2797892f0366e1a1ff07a5c3f2
raghavendra09 <raghavendratirunagari.hadoop@gmail.com>
2017-12-25 16:05:54 +0530

    Add files via upload

---
+++ DocumentationComments/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.dbcore.DocumentationComments;
+
+/**
+ * Project: CoreJava
+ * Author: test
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        /* Prints Hello, World! on standard output. */
+        System.out.println("Hello World!");
+    }
+}
