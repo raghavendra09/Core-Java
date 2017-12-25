31065792cc5168ce65515926b3c2aa8ec042401a
raghavendra09 <raghavendratirunagari.hadoop@gmail.com>
2017-12-25 16:08:57 +0530

    Add files via upload

---
+++ Generics/Box.java
@@ -0,0 +1,31 @@
+package com.dbcore.Generics;
+
+/**
+ * Project: CoreJava
+ * Author: test
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class Box<T> {
+
+    private T t;
+
+    public void add(T t) {
+        this.t = t;
+    }
+
+    public T get() {
+        return t;
+    }
+
+    public static void main(String[] args) {
+        Box<Integer> integerBox = new Box<Integer>();
+        Box<String> stringBox = new Box<String>();
+
+        integerBox.add(new Integer(10));
+        stringBox.add(new String("Hello World"));
+
+        System.out.printf("Integer Value :%d\n\n", integerBox.get());
+        System.out.printf("String Value :%s\n", stringBox.get());
+    }
+}
