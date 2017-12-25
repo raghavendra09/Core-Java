1fe9422cc339be3d0f2acc2f7cc8d93b376a6988
raghavendra09 <raghavendratirunagari.hadoop@gmail.com>
2017-12-25 16:18:45 +0530

    Add files via upload

---
+++ Serialization/Employee.java
@@ -0,0 +1,20 @@
+package com.dbcore.Serialization;
+
+/**
+ * Project: CoreJava
+ * Author: test
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class Employee implements java.io.Serializable
+{
+    public String name;
+    public String address;
+    public transient int SSN;
+    public int number;
+    public void mailCheck()
+    {
+        System.out.println("Mailing a check to " + name
+                + " " + address);
+    }
+}
