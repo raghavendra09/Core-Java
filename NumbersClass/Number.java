409ce1302c807cdfa9bfa70d53c54466542ed3bd
raghavendra09 <raghavendratirunagari.hadoop@gmail.com>
2017-12-25 16:15:44 +0530

    Add files via upload

---
+++ NumbersClass/Number.java
@@ -0,0 +1,18 @@
+package com.dbcore.NumbersClass;
+
+/**
+ * Project: CoreJava
+ * FileName: test
+ * Author: test
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class Number {
+
+    public static void main(String args[]) {
+        Integer x = 5; // boxes int to an Integer object
+        x = x + 10;   // unboxes the Integer to a int
+
+        System.out.println(x);
+    }
+}
