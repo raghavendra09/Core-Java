4a29afeade143dafe4c9cc7df8b25c4154de93e4
raghavendra09 <raghavendratirunagari.hadoop@gmail.com>
2017-12-25 15:58:34 +0530

    Add files via upload

---
+++ Abstraction/AbstractDemo.java
@@ -0,0 +1,23 @@
+package com.dbcore.Abstraction;
+
+/**
+ * Project: CoreJava
+ * Author: test
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+/* File name : AbstractDemo.java */
+public class AbstractDemo
+{
+    public static void main(String [] args)
+    {
+        Salary s = new Salary("Mohd Mohtashim", "Ambehta, UP", 3, 3600.00);
+        Employee e = new Salary("John Adams", "Boston, MA", 2, 2400.00);
+
+        System.out.println("Call mailCheck using Salary reference --");
+        s.mailCheck();
+
+        System.out.println("\n Call mailCheck using Employee reference--");
+        e.mailCheck();
+    }
+}
